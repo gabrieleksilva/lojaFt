7f1cb65bbea9fb8de01b596bc810e9b0dd0d7c2f
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2025-05-16 23:59:26 -0300

    integracao com backend para a tela contato

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.css']
 })
-export class ContatoComponent implements OnInit {
+export class ContatoComponent {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.form = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mensagem: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    if (this.form.valid) {
+      this.http.post('http://localhost:8080/loja/contato', this.form.value).subscribe({
+        next: () => {
+          alert('Mensagem enviada com sucesso!');
+          this.form.reset();
+        },
+        error: err => {
+          console.error(err);
+          alert('Erro ao enviar a mensagem.');
+        }
+      });
+    }
+  }
 }
